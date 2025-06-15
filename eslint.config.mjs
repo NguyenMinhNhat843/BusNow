8ab0c50543754a8bfce6d225cab114cfe0292513
NguyenMinhNhat843 <NhatNguyen15062019@gmail.com>
2025-06-15 08:03:45 +0700

    setup client with nextjs + tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "off", // Tắt check biến chưa sử dụng
+      "@typescript-eslint/no-unused-vars": "off", // Tắt no-unused-vars cho TypeScript
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,149 @@
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faCircleDot,
+  faLocationDot,
+  faRightLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { vi } from "date-fns/locale";
 
-export default function Home() {
+const vehicle = [
+  {
+    id: 1,
+    name: "Xe khách",
+    // icon:
+  },
+  {
+    id: 2,
+    name: "Máy bay",
+    // icon:
+  },
+  {
+    id: 3,
+    name: "Tàu hỏa",
+    // icon:
+  },
+  {
+    id: 4,
+    name: "Thuê xe",
+    // icon:
+  },
+];
+
+export default function HomePage() {
+  const [vehicleTab, setVehicleTab] = useState("Xe khách");
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main className="">
+      {/* SearchBar */}
+      <div className="relative h-[400px]">
+        <div className="absolute w-full h-full">
+          <Image
+            src="/bg_searchbar.jpg"
+            alt="Backgorund searchbar"
+            fill
+            style={{ objectFit: "cover" }}
+            priority // nếu bạn muốn load ảnh ngay lập tức (ví dụ background trang chính)
+          />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {/* Overlay */}
+        <div className="absolute w-full h-full flex flex-col items-center justify-center bg-black/50">
+          <h1 className="text-white text-2xl ">
+            Đặt vé dễ dàng - nhanh chóng - tiện lợi
+          </h1>
+          <form
+            action=""
+            className="bg-white rounded-2xl shadow-lg px-4 py-4 max-w-5xl w-full mx-auto"
+          >
+            {/* Tab vehicle */}
+            <div className="flex justify-center items-center gap-6 pb-4">
+              {vehicle.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={`cursor-pointer ${
+                      item.name === vehicleTab
+                        ? "font-bold border-b-2 border-blue-500"
+                        : ""
+                    }`}
+                    onClick={() => setVehicleTab(item.name)}
+                  >
+                    <p>{item.name}</p>
+                  </div>
+                );
+              })}
+            </div>
+            {/* end: Tab vehicle */}
+
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex justify-between gap-4 grow border border-slate-300 rounded-2xl p-2">
+                {/* Nơi đi */}
+                <div className="flex items-center justify-between gap-4">
+                  <FontAwesomeIcon
+                    icon={faCircleDot}
+                    className="text-3xl text-blue-600"
+                  />
+                  <div className="cursor-pointer">
+                    <p className="text-sm text-slate-500">Nơi xuất phát</p>
+                    <p>Sài gòn</p>
+                  </div>
+                </div>
+
+                {/* Nút chuyển */}
+                <div className="flex justify-center items-center bg-slate-300 rounded-full w-10 h-10 cursor-pointer">
+                  <FontAwesomeIcon icon={faRightLeft} />
+                </div>
+
+                {/* Nơi đến */}
+                <div className="flex items-center justify-between gap-4">
+                  <FontAwesomeIcon
+                    icon={faLocationDot}
+                    className="text-3xl text-red-600"
+                  />
+                  <div className="cursor-pointer">
+                    <p className="text-sm text-slate-500">Nơi đến</p>
+                    <p>Nha Trang</p>
+                  </div>
+                </div>
+
+                {/* Ngày đi */}
+                <div className="flex items-center justify-between gap-4">
+                  <FontAwesomeIcon
+                    icon={faCalendar}
+                    className="text-3xl text-blue-600"
+                  />
+                  <div className="cursor-pointer">
+                    <p className="text-sm text-slate-500">Ngày đi</p>
+                    <div>
+                      <DatePicker locale={vi} className="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* Nút Tìm kiếm */}
+              <div className="bg-yellow-500 cursor-pointer text-white  rounded-lg hover:bg-yellow-700 transition-colors">
+                <p className="text-xl text-center text-black px-4 py-4 w-[200px] ">
+                  Tìm kiếm
+                </p>
+              </div>
+              {/* end */}
+            </div>
+          </form>
+        </div>
+      </div>
+      {/* end: search bar */}
+
+      {/* Tuyến đường phổ biến */}
+      <div></div>
+      {/* end: Tuyến đường phổ biến */}
+    </main>
   );
 }
